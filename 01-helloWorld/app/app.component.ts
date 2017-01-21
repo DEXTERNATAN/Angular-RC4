460ae09dd9cff1a640d74353bfaf18c00c126493
Rafael Carvalho <rapha.pse@outlook.com>
2017-01-21 15:17:24 -0200

    Verificando

--- 01-helloWorld/app/app.component.ts
+++ 01-helloWorld/app/app.component.ts
@@ -5,6 +5,5 @@ import { Component } from '@angular/core';
     template: `
         <h1>Projeto Angular 2 - Curso de Angular</h1>
         <p>Olá Mundo!</p>
-    `
 })
 export class AppComponent { }
