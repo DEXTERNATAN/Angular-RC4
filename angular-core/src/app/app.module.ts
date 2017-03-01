4a1bd4d74b889ae205b55ef5662eafed880db771
Rafael <rapha.pse@outlook.com>
2017-03-01 16:31:09 -0300

    Referenciando paginas

--- angular-core/src/app/app.module.ts
+++ angular-core/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PaginasComponent } from './paginas/paginas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginasComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-core/src/app/paginas/paginas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'paginas',
+  templateUrl: './paginas.component.html',
+  styleUrls: ['./paginas.component.css']
+})
+export class PaginasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
